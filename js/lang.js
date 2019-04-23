a6c85326040af13290afe9f7144dc1648dcb42a4
angad128 <ango1993a@gmail.com>
2019-04-23 20:22:44 +0545

    Language buttom added

--- js/lang.js
+++ js/lang.js
@@ -1,4 +1,7 @@
-  		var arrLang = {
+  	  // १ २ ३ ४ ५ ६ ७ ८ ९ ० 
+
+
+    	var arrLang = {
   			'en' : {
   				"home": "Home",
   				"about" : "About",
@@ -6,7 +9,15 @@
 		        "news" : "News",
 		        "boardmember" : "Boardmember",
 		        "gallery": "Gallery",
-		        "contact": "Contact"
+		        "contact": "Contact",
+            "location": "Our Location",
+
+            "conatct-details": "Contact Details",
+            "place": "Kathmandu, Nepal",
+            "telephone": "Telephone: +977-01-4445789",
+            "fax": "Fax:  +977-01-4412426",
+            "pbo": "P.B.No.: +977-01-23264",
+      
   			},
   			'np' : {
   				"home": "घर",
@@ -15,7 +26,14 @@
 		        "news" : "समाचार",
 		        "boardmember" : "कार्यसमिति",
 		        "gallery": "गैलरी",
-		        "contact": "सम्पर्क"
+		        "contact": "सम्पर्क",
+            "location": "हाम्रो स्थान",
+
+            "conatct-details": "सम्पर्क",
+            "place": "काठमाडौं, नेपाल",
+            "telephone": "टेलिफोन: +९७७-०१-४४४५७८९",
+            "fax": "फैक्स:  +९७७-०१-४४१२४२६",
+            "pbo": "P.B.No.: +९७७-०१-२३२६४",
   			}
   		}
 
